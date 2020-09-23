0d3f8e1ea3cfb5720ebbab8f06aadc0adf038fd2
marcio-martins <a.marcio.sm@gmail.com>
2020-09-23 13:50:08 -0300

    Methods run(), start() and sleep() of Thread.

---
+++ src/main/java/thread/MyThread.java
@@ -0,0 +1,31 @@
+package thread;
+
+public class MyThread extends Thread {
+
+	private String name;
+	private Integer time;
+	
+	public MyThread(String name) {
+		this.name = name;
+		this.time = 100;
+	}
+	
+	public MyThread(String name, Integer time) {
+		this.name = name;
+		this.time = time;
+	}
+	
+	public void run() {
+		
+		for(int i=0; i<5 ; i++) {
+			System.out.println(name + " #" + i);
+			try {
+				Thread.sleep(time);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
---
+++ src/main/java/thread/ThreadGo.java
@@ -0,0 +1,15 @@
+package thread;
+
+public class ThreadGo {
+
+	public static void main(String[] args) {
+		MyThread myThread1 = new MyThread("First",1000);
+		MyThread myThread2 = new MyThread("Second",500);
+		MyThread myThread3 = new MyThread("Third");
+		
+		myThread1.start(); // no run(), but yes start()
+		myThread2.start();
+		myThread3.start();
+	}
+
+}
