d64cd60a4901459b1c93388900d26035ffa1c14d
marcio-martins <a.marcio.sm@gmail.com>
2020-09-26 23:23:49 -0300

    Thread priority

--- src/main/java/thread/ThreadGo.java
+++ src/main/java/thread/ThreadGo.java
@@ -3,11 +3,16 @@ package thread;
 public class ThreadGo {
 
 	public static void main(String[] args) {
-		MyThread myThread1 = new MyThread("First",1000);
+		MyThread myThread1 = new MyThread("First",500);
 		MyThread myThread2 = new MyThread("Second",500);
-		MyThread myThread3 = new MyThread("Third");
+		MyThread myThread3 = new MyThread("Third",500);
 		
-		myThread1.start(); // no run(), but yes start()
+		myThread1.setPriority(Thread.NORM_PRIORITY); // numbers from 1 to 10
+		myThread2.setPriority(Thread.MAX_PRIORITY);
+		myThread2.setPriority(10);
+		myThread3.setPriority(Thread.MIN_PRIORITY);
+		
+		myThread1.start();
 		myThread2.start();
 		myThread3.start();
 	}
