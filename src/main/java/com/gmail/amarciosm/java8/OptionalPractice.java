c55c0becf22e99695f8e5fb85211d89dbf1ac9c8
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2022-11-26 12:00:46 -0300

    Optional examples.

--- src/main/java/com/gmail/amarciosm/java8/OptionalPractice.java
+++ src/main/java/com/gmail/amarciosm/java8/OptionalPractice.java
@@ -1,6 +1,9 @@
 package com.gmail.amarciosm.java8;
 
 import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
 
 public class OptionalPractice {
 
@@ -20,6 +23,27 @@ public class OptionalPractice {
 		System.out.println(integerOptional);
 		integerOptional = Optional.ofNullable(integer);
 		System.out.println(integerOptional);
+		
+		
+		if(integerOptional.isPresent()) {
+			System.out.println(integerOptional.get());
+		}
+		
+		integerOptional.ifPresent(i -> System.out.println(i));
+		
+		integerOptional.orElse(2);
+		
+		integerOptional.orElseGet(() -> 1+1+1);
+		
+		try {
+			integerOptional.orElseThrow(() -> new Exception("Oh no!"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		OptionalInt i = OptionalInt.of(0);
+		OptionalDouble d = OptionalDouble.empty();
+		OptionalLong l = OptionalLong.of(0L);
 	}
 	
 }
