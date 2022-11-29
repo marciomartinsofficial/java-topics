b23eef36da99e328b6ca93f02441a7e2c851409c
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2022-11-29 18:10:00 -0300

    Collect in Stream - Collectors.joining

--- src/main/java/com/gmail/amarciosm/java8/StreamCollectPractice.java
+++ src/main/java/com/gmail/amarciosm/java8/StreamCollectPractice.java
@@ -3,6 +3,7 @@ package com.gmail.amarciosm.java8;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class StreamCollectPractice {
@@ -18,6 +19,20 @@ public class StreamCollectPractice {
 		Set<Integer> set = integerList.stream()
 				.collect(Collectors.toSet());
 		System.out.println(set);
+		
+		TreeSet<Integer> treeSet = integerList.stream()
+				.collect(Collectors.toCollection(() -> new TreeSet<>())); // any collect
+		System.out.println(treeSet);
+		
+		String joing = integerList.stream()
+				.map(n -> n.toString())
+				.collect(Collectors.joining());
+		System.out.println(joing);
+		
+		String joingHyphen = integerList.stream()
+				.map(n -> n.toString())
+				.collect(Collectors.joining(" - "));
+		System.out.println(joingHyphen);		
 	}
 	
 }
