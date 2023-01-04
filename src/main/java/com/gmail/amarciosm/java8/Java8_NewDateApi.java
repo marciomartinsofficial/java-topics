fff3d51acd943168b4a75306acdaf98b09cf56f6
marcio.martins@sefin.fortaleza.ce.gov.br <a.marcio.sm@gmail.com>
2023-01-04 20:24:32 -0300

    Java8 - NewDateApi - Period

--- src/main/java/com/gmail/amarciosm/java8/Java8_NewDateApi.java
+++ src/main/java/com/gmail/amarciosm/java8/Java8_NewDateApi.java
@@ -4,10 +4,34 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.Period;
 
 public class Java8_NewDateApi {
 
 	public static void main(String[] args) {
+		//partOne();
+		partTwo();
+	}
+	
+	private static void partTwo() {
+		Period period = Period.of(2, 3, 1);
+		System.out.println(period);
+		
+		LocalDate localDate1 = LocalDate.of(2022, Month.JANUARY, 1);
+		LocalDate localDate2 = localDate1.plusMonths(2);
+		period = Period.between(localDate1, localDate2);
+		System.out.println(period);
+		
+		period = localDate1.until(localDate2);
+		System.out.println(period);
+		
+		LocalDate birthday = LocalDate.of(1987, 3, 24);
+		LocalDate today = LocalDate.now();
+		period = Period.between(birthday, today);
+		System.out.println(period);
+	}
+
+	private static void partOne() {
 		LocalDate localDate = LocalDate.of(2022, 12, 20);
 		System.out.println(localDate);
 		
